9121b35063f3d5f71680904f905e891bc6d5c733
Joseph Chang <joseph.chang@vanderbilt.edu>
2021-04-05 14:21:10 -0500

    updating repo

---
+++ cmake-build-debug/CMakeFiles/CS3258_cmake.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/CLI.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/CLI.cpp.obj"
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/Command.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/Command.cpp.obj"
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/EndianChecks.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/EndianChecks.cpp.obj"
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/Filter.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/Filter.cpp.obj"
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/ResizeCommand.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/ResizeCommand.cpp.obj"
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/TiffImageData.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/TiffImageData.cpp.obj"
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/TiffReadCommand.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/TiffReadCommand.cpp.obj"
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/TiffStatCommand.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/TiffStatCommand.cpp.obj"
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/TiffWriteCommand.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/TiffWriteCommand.cpp.obj"
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/Tokenizer.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/Tokenizer.cpp.obj"
+  "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/main.cpp" "C:/Users/chang/Desktop/cs3258/josephsgraphiccode/cmake-build-debug/CMakeFiles/CS3258_cmake.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CS3258_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS3258_cmake.dir/CLI.cpp.obj"
+  "CMakeFiles/CS3258_cmake.dir/Command.cpp.obj"
+  "CMakeFiles/CS3258_cmake.dir/EndianChecks.cpp.obj"
+  "CMakeFiles/CS3258_cmake.dir/Filter.cpp.obj"
+  "CMakeFiles/CS3258_cmake.dir/ResizeCommand.cpp.obj"
+  "CMakeFiles/CS3258_cmake.dir/TiffImageData.cpp.obj"
+  "CMakeFiles/CS3258_cmake.dir/TiffReadCommand.cpp.obj"
+  "CMakeFiles/CS3258_cmake.dir/TiffStatCommand.cpp.obj"
+  "CMakeFiles/CS3258_cmake.dir/TiffWriteCommand.cpp.obj"
+  "CMakeFiles/CS3258_cmake.dir/Tokenizer.cpp.obj"
+  "CMakeFiles/CS3258_cmake.dir/main.cpp.obj"
+  "CS3258_cmake.exe"
+  "CS3258_cmake.exe.manifest"
+  "CS3258_cmake.pdb"
+  "libCS3258_cmake.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS3258_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
